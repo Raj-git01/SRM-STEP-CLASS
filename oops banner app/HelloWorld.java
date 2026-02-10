460593d692cba1168729f28e46466d1990b8f322
Raj <raj20063251@gmail.com>
2026-02-10 15:47:58 +0530

    updatesssss

--- oops banner app/HelloWorld.java
+++ oops banner app/HelloWorld.java
@@ -1,5 +1,11 @@
-class banner{
+class Banner{
     void show()
     {
         System.out.println("Welcome");
     }
+    public static void main (String[] args)
+    {
+        Banner banner1 = new Banner();
+        banner1.show();
+    }
+}
