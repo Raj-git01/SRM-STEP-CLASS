62fc2af6d2807c3dedf0e0ef1dfe50e4093f4567
Raj <raj20063251@gmail.com>
2026-02-10 15:38:46 +0530

    trial

--- oops banner app/HelloWorld.java
+++ oops banner app/HelloWorld.java
@@ -3,7 +3,4 @@ class banner{
     {
         System.out.println("Welcome");
     }
-    public static void main (String[] args)
-    {
-        banner banner1 = new banner();
-        banner1.show();
+   
